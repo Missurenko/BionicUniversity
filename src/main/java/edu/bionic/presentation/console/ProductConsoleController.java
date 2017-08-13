ab73930a07949985319332b10a64be5aab4db774
B.missurenko <b.missurenko@gmail.com>
2017-08-13 13:29:55 +0300

    topic 2Hw

--- src/main/java/edu/bionic/presentation/console/ProductConsoleController.java
+++ src/main/java/edu/bionic/presentation/console/ProductConsoleController.java
@@ -1,5 +1,6 @@
 package edu.bionic.presentation.console;
 
+import edu.bionic.domain.Product;
 import edu.bionic.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,5 +17,15 @@ public class ProductConsoleController {
 
     public void printAllProducts() {
         productService.getAll().forEach(product -> System.out.println(product.printInfo()));
+    }
+
+    public void printProductInfo(int productId) {
+        Product product = productService.getById(productId);
+
+        System.out.println("Наименование: " + product.getName());
+        System.out.println("Цвет: " + product.getColor());
+        System.out.println("Память: " + product.getCapacity());
+        System.out.println("Дисплей: " + product.getDisplay());
+        System.out.println("Цена: " + product.getPrice());
     }
 }
