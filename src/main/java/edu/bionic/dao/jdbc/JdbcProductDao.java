a29753ed3fb12a11b6ebabb6e24aeff769f7df01
B.missurenko <b.missurenko@gmail.com>
2017-08-13 13:34:20 +0300

    topic 8

--- src/main/java/edu/bionic/dao/jdbc/JdbcProductDao.java
+++ src/main/java/edu/bionic/dao/jdbc/JdbcProductDao.java
@@ -9,12 +9,14 @@ import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 @Primary
+@Transactional
 public class JdbcProductDao implements ProductDao {
 
     private RowMapper<Product> ROW_MAPPER;
@@ -50,5 +52,11 @@ public class JdbcProductDao implements ProductDao {
         String sql = "SELECT * FROM products WHERE id = ?";
         List<Product> product = jdbcTemplate.query(sql, new Object[]{productId}, ROW_MAPPER);
         return Optional.ofNullable(DataAccessUtils.singleResult(product));
+    }
+
+    List<Product> getByOrder(int orderId) {
+        String sql = "SELECT * FROM products LEFT JOIN orders_products ON products.id = orders_products.product_id " +
+                "WHERE orders_products.order_id = ?";
+        return jdbcTemplate.query(sql, new Object[] {orderId},ROW_MAPPER);
     }
 }
